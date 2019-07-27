c5e4f760249fe7460f9e95edb950af237c5e1a11
millboh <millbo@gmail.com>
2019-07-27 21:14:52 +0200

    #59 renamed TO_DATE udf

--- runner/src/main/java/org/ananas/runner/steprunner/sql/SQLTransformer.java
+++ runner/src/main/java/org/ananas/runner/steprunner/sql/SQLTransformer.java
@@ -29,11 +29,6 @@ public class SQLTransformer extends TransformerStepRunner {
             .apply(
                 "sql transform",
                 SqlTransform.query(statement)
-                    .registerUdf("falseIfNull", new NullableBooleanFn())
-                    .registerUdf("zeroIfNull", new NullableBigDecimalFn())
-                    .registerUdf("zeroIfNull", new NullableIntegerFn())
-                    .registerUdf("emptyIfNull", new NullableStringFn())
-                    .registerUdf("hash", new HashFn())
-                    .registerUdf("todate", ToDateFn.class));
+                    .registerUdf("TO_DATE", ToDateFn.class));
   }
 }
