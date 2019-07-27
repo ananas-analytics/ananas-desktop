a8d70d23b16e147a5377e41c5707c655512d1342
Bo HOU <daily.bhou@gmail.com>
2019-07-27 20:55:54 +0200

    update version to 0.9.0

--- ui/src/common/util/update.js
+++ ui/src/common/util/update.js
@@ -22,7 +22,7 @@ function checkUpdate() {
   // TODO: better way to handle the update on linux
   if (version === '0.0.1') {
     // remember to update this version when having new release
-    version = '0.8.3'
+    version = '0.9.0'
   }
 
   let hash = 99
