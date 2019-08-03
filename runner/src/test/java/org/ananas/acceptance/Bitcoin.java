451208c7a3e4cb4dd584c24482abee41f776b893
Bo HOU <daily.bhou@gmail.com>
2019-08-03 13:48:55 +0200

    fix java format

--- runner/src/test/java/org/ananas/acceptance/Bitcoin.java
+++ runner/src/test/java/org/ananas/acceptance/Bitcoin.java
@@ -59,33 +59,34 @@ public class Bitcoin {
     exit.expectSystemExitWithStatus(0);
 
     exit.checkAssertionAfterwards(
-      new Assertion() {
-        public void checkAssertion() {
-          String json = systemOutRule.getLog();
-          int code = JsonPath.read(json, "$.code");
-          Assert.assertEquals(200, code);
+        new Assertion() {
+          public void checkAssertion() {
+            String json = systemOutRule.getLog();
+            int code = JsonPath.read(json, "$.code");
+            Assert.assertEquals(200, code);
 
-          List<Map<String, String>> fields = JsonPath.read(json, "$.data.5d4557439c7a5441fdc67d3b.schema.fields");
-          Assert.assertTrue(fields.size() > 0);
+            List<Map<String, String>> fields =
+                JsonPath.read(json, "$.data.5d4557439c7a5441fdc67d3b.schema.fields");
+            Assert.assertTrue(fields.size() > 0);
 
-          Assert.assertEquals("code", fields.get(0).get("name"));
-          Assert.assertEquals("VARCHAR", fields.get(0).get("type"));
+            Assert.assertEquals("code", fields.get(0).get("name"));
+            Assert.assertEquals("VARCHAR", fields.get(0).get("type"));
 
-          Assert.assertEquals("rate", fields.get(1).get("name"));
-          Assert.assertEquals("DECIMAL", fields.get(1).get("type"));
+            Assert.assertEquals("rate", fields.get(1).get("name"));
+            Assert.assertEquals("DECIMAL", fields.get(1).get("type"));
 
-          Assert.assertEquals("time", fields.get(2).get("name"));
-          Assert.assertEquals("VARCHAR", fields.get(2).get("type"));
-        }
-      });
+            Assert.assertEquals("time", fields.get(2).get("name"));
+            Assert.assertEquals("VARCHAR", fields.get(2).get("type"));
+          }
+        });
 
     ClassLoader classLoader = getClass().getClassLoader();
     URL project = classLoader.getResource("test_projects/bitcoin");
 
     Main.main(
-      new String[] {
-        "test", "-p", project.getPath(), "5d4557439c7a5441fdc67d3b",
-      });
+        new String[] {
+          "test", "-p", project.getPath(), "5d4557439c7a5441fdc67d3b",
+        });
   }
 
   @Test
@@ -93,23 +94,23 @@ public class Bitcoin {
     exit.expectSystemExitWithStatus(0);
 
     exit.checkAssertionAfterwards(
-      new Assertion() {
-        public void checkAssertion() {
-          String json = systemOutRule.getLog();
-          int code = JsonPath.read(json, "$.code");
-          Assert.assertEquals(200, code);
+        new Assertion() {
+          public void checkAssertion() {
+            String json = systemOutRule.getLog();
+            int code = JsonPath.read(json, "$.code");
+            Assert.assertEquals(200, code);
 
-          String jobId = JsonPath.read(json, "$.data.jobid");
-          Assert.assertNotNull(jobId);
-        }
-      });
+            String jobId = JsonPath.read(json, "$.data.jobid");
+            Assert.assertNotNull(jobId);
+          }
+        });
 
     ClassLoader classLoader = getClass().getClassLoader();
     URL project = classLoader.getResource("test_projects/bitcoin");
 
     Main.main(
-      new String[] {
-        "run", "-p", project.getPath(), "5d4559249c7a5441fdc67d47",
-      });
+        new String[] {
+          "run", "-p", project.getPath(), "5d4559249c7a5441fdc67d47",
+        });
   }
 }
