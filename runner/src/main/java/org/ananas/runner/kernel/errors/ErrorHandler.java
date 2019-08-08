2160290134d3ae8a098423e86bf315768471d9cb
Bo HOU <daily.bhou@gmail.com>
2019-08-08 22:16:36 +0200

    fix excel explore write to original file issue

--- runner/src/main/java/org/ananas/runner/kernel/errors/ErrorHandler.java
+++ runner/src/main/java/org/ananas/runner/kernel/errors/ErrorHandler.java
@@ -2,6 +2,8 @@ package org.ananas.runner.kernel.errors;
 
 import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +15,11 @@ public class ErrorHandler implements Serializable {
   private static int DEFAULT_MAX_ERRORS = Integer.MAX_VALUE;
 
   private AtomicInteger count;
+  private AtomicReference<String> lastErrorMessage;
 
   protected ErrorHandler(int maxError) {
     this.count = new AtomicInteger(0);
+    this.lastErrorMessage = new AtomicReference<>("");
   }
 
   public ErrorHandler() {
@@ -24,6 +28,7 @@ public class ErrorHandler implements Serializable {
 
   public void addError(ExceptionHandler.ErrorCode code, String message) {
     int c = this.count.incrementAndGet();
+    this.lastErrorMessage.set(message);
     LOG.debug(message);
     if (c >= DEFAULT_MAX_ERRORS) {
       throw ExceptionHandler.valueOf(code, message);
@@ -32,9 +37,20 @@ public class ErrorHandler implements Serializable {
 
   public void addError(Exception e) {
     int c = this.count.incrementAndGet();
+    this.lastErrorMessage.set(e.getLocalizedMessage());
     LOG.debug(e.getMessage());
     if (c >= DEFAULT_MAX_ERRORS) {
       throw ExceptionHandler.valueOf(e);
     }
+  }
+
+  public boolean hasError() {
+    return count.get() > 0;
+  }
+
+  @Override
+  public String toString() {
+    return "Encounter " + count.get() +
+      " errors, lastErrorMessage=" + lastErrorMessage.get();
   }
 }
