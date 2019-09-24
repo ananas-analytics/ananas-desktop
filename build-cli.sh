553a4c8615b1324cf42c788f4eecd21ffa452d83
Bo HOU <daily.bhou@gmail.com>
2019-09-25 00:30:44 +0200

    Use plain console output for build-cli script

--- build-cli.sh
+++ build-cli.sh
@@ -14,18 +14,18 @@ mkdir -p artifacts/extension
 if [ -z "$1" ]
 then
     echo "use default jdk to build CLI"
-    ./gradlew :runner:shadowJar -Dtarget=cli -Prelease=true
+    ./gradlew :runner:shadowJar -Dtarget=cli -Prelease=true --console plain
 else
     echo "use jdk '$1' to build CLI"
-    ./gradlew :runner:shadowJar -Dorg.gradle.java.home=$1 -Dtarget=cli -Prelease=true
+    ./gradlew :runner:shadowJar -Dorg.gradle.java.home=$1 -Dtarget=cli -Prelease=true --console plain
 fi
 
 cp runner/build/libs/ananas-cli*.jar ./cli
 cp $(ls ./cli/*.jar) ./cli/ananas-cli-latest.jar
 
 # build artifacts
-./gradlew :runner:extensionJar -Prelease=true
-./gradlew :runner:engineJar -Prelease=true -Dengine=spark
+./gradlew :runner:extensionJar -Prelease=true --console plain
+./gradlew :runner:engineJar -Prelease=true -Dengine=spark --console plain
 
 cp ./runner/build/libs/ananas-spark* ./artifacts/engine/spark
 cp $(ls ./runner/build/libs/ananas-spark*.jar) ./runner/build/libs/ananas-spark-latest.jar
